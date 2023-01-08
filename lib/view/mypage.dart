49500eb68df3dd5929e9eba87d62659aeea12a89
YunHyeonJeong <wjd6408@gmail.com>
2023-01-08 23:45:25 +0900

    [UPDATE] 마이페이지 View 수정

--- lib/view/mypage.dart
+++ lib/view/mypage.dart
@@ -42,8 +42,15 @@ class MyPage extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Container(
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: const Color(0xFF99CD89),
+          borderRadius: BorderRadius.circular(5),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 1,
+              blurRadius: 1,
+            ),
+          ],
         ),
         width: Get.width,
         child: Padding(
@@ -52,17 +59,17 @@ class MyPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const SizedBox(height: 10),
-              _profileContent('이름', '정윤현', 4),
+              _profileContent('이름', '홍길동', 4),
               const SizedBox(height: 10),
-              const Divider(thickness: 0.5, height: 1, color: Colors.black),
+              const Divider(thickness: 0.5, height: 1, color: Colors.grey),
               const SizedBox(height: 10),
               _profileContent('성별', '남', 4),
               const SizedBox(height: 10),
-              const Divider(thickness: 0.5, height: 1, color: Colors.black),
+              const Divider(thickness: 0.5, height: 1, color: Colors.grey),
               const SizedBox(height: 10),
               _profileContent('생년월일', '1996-01-26', 5.2),
               const SizedBox(height: 10),
-              const Divider(thickness: 0.5, height: 1, color: Colors.black),
+              const Divider(thickness: 0.5, height: 1, color: Colors.grey),
               const SizedBox(height: 10),
               _profileContent('이메일', 'test@naver.com', 4.5),
               const SizedBox(height: 10),
@@ -97,23 +104,89 @@ class MyPage extends StatelessWidget {
     );
   }
 
+  Widget _btnContent() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 27),
+      child: Column(
+        children: <Widget>[
+          Card(
+            elevation: 4,
+            child: Column(
+              children: <Widget>[
+                _btnContentActions(
+                  "컨텐츠1",
+                  const Icon(
+                    Icons.mail_outline,
+                    color: Color(0xFFF9893E),
+                  ),
+                ),
+                const Divider(),
+                _btnContentActions(
+                  "컨텐츠2",
+                  const Icon(
+                    Icons.payment,
+                    color: Color(0xFFF9893E),
+                  ),
+                ),
+                const Divider(),
+                _btnContentActions(
+                  "컨텐츠3",
+                  const Icon(
+                    Icons.help_outline,
+                    color: Color(0xFFF9893E),
+                  ),
+                ),
+                const Divider(),
+                _btnContentActions(
+                  "컨텐츠4",
+                  const Icon(
+                    Icons.info_outline,
+                    color: Color(0xFFF9893E),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _btnContentActions(String text, Icon icon) {
+    return ListTile(
+      leading: icon == null ? const Icon(Icons.error) : icon,
+      title: Text(text),
+      trailing: const Icon(
+        Icons.arrow_forward_ios,
+        size: 15,
+        color: Colors.grey,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('MyPage'),
+        title: const Text('마이 페이지'),
         elevation: 0,
       ),
-      body: Column(
-        children: [
-          const SizedBox(height: 30),
-          _head('기본정보'),
-          const SizedBox(height: 1),
-          _profile(),
-          const SizedBox(height: 30),
-          _head('추가정보'),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 30),
+            _head('기본정보'),
+            const SizedBox(height: 3),
+            _profile(),
+            const SizedBox(height: 30),
+            _head('추가정보'),
+            _btnContent(),
+            const SizedBox(height: 30),
+            _head('사용자정보'),
+            _btnContent(),
+          ],
+        ),
       ),
     );
   }
