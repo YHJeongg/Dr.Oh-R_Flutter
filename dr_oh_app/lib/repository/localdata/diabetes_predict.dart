fa49fdbb6f8603c33ec4da2f0d3ac2b49765b64f
MirrorSpring <dbtmd619@naver.com>
2023-01-11 18:45:39 +0900

    당뇨병, 뇌졸중 진단결과 DB에 저장

--- dr_oh_app/lib/repository/localdata/diabetes_predict.dart
+++ dr_oh_app/lib/repository/localdata/diabetes_predict.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dr_oh_app/model/diabetes_message.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DiabetesPredict {
   //Desc: 당뇨병 예측
@@ -44,6 +46,23 @@ class DiabetesPredict {
     DiabetesMessage.physhealth.text='';
     DiabetesMessage.isComplete=false;
 
+    _saveResult(result);
+
     return result;
+  }
+
+    //Desc: 검사 결과 저장
+  //Date: 2023-01-11
+  _saveResult(String result) async {
+    final prefs = await SharedPreferences.getInstance();
+    String id = prefs.getString('id')!;
+
+    var doc1=await FirebaseFirestore.instance.collection('users').where('id',isEqualTo: id).get();
+    var doc2=doc1.docs.first.id;
+
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(doc2)
+        .set({'Diabetes': result});
   }
 }
--- dr_oh_app/lib/repository/localdata/stroke_predict.dart
+++ dr_oh_app/lib/repository/localdata/stroke_predict.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class StrokePredict {
   //Date: 2023-01-10, SangwonKim
@@ -40,6 +42,23 @@ class StrokePredict {
     var dataConvertedJSON = json.decode(utf8.decode(response.bodyBytes));
     String result = dataConvertedJSON['result'];
 
+    _saveResult(result);
+    
     return result;
+  }
+
+      //Desc: 검사 결과 저장
+  //Date: 2023-01-11
+  _saveResult(String result) async {
+    final prefs = await SharedPreferences.getInstance();
+    String id = prefs.getString('id')!;
+
+    var doc1=await FirebaseFirestore.instance.collection('users').where('id',isEqualTo: id).get();
+    var doc2=doc1.docs.first.id;
+
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(doc2)
+        .set({'Stroke': result});
   }
 }
